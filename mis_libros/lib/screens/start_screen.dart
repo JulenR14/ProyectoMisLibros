c40551d600ac26bbe933dd056ae14d0d22d62465
JulenR14 <julen20191414@gmail.com>
2024-02-16 18:32:18 +0100

    Sigo implementando la start_screen

--- mis_libros/lib/screens/start_screen.dart
+++ mis_libros/lib/screens/start_screen.dart
@@ -15,15 +15,16 @@ class _StartScreen extends State<StartScreen> {
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     return Scaffold(
+      backgroundColor: Colors.marron5,
         body:  SingleChildScrollView(
           child: Container(
             color: Colors.marron5,
-            height: MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top,
+            height: MediaQuery.of(context).size.height,
             width: MediaQuery.of(context).size.width,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Image.asset('assets/images/logo.png',
+                Image.asset('assets/images/Logo.png',
                 scale: 3.5),
                 const SizedBox(height: 20),
                 const Padding(padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
