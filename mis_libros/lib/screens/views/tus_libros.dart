53bde09650b4abbb80654a6cda95a2759d59b99c
JulenR14 <144845185+JulenR14@users.noreply.github.com>
2024-02-27 20:15:48 +0100

    He arreglado el nombre de las cards de los libros, ahora puede ser todo lo largo que sea

--- mis_libros/lib/screens/views/tus_libros.dart
+++ mis_libros/lib/screens/views/tus_libros.dart
@@ -13,37 +13,37 @@ class _TusLibros extends State<TusLibros> {
   //libros que utlizamos para comprobar como se ve la lista de libros
   List<Libro> listaLibros = [
     Libro(
-        'El inversor\ninteligente',
+        'El inversor inteligente',
         'Benjamin Graham',
         'assets/images/librosPrueba/book_inversorInteligente.jpg',
         4.5,
         "El inversor inteligente es un libro de inversión escrito por Benjamin Graham en 1949. El libro es ampliamente considerado como uno de los mejores libros de inversión de todos los tiempos. Graham propone un método para encontrar empresas infravaloradas y actuar en consecuencia."),
     Libro(
-        'Padre rico,\npadre pobre',
+        'Padre rico, padre pobre',
         'Robert Kiyosaki',
         'assets/images/librosPrueba/book_padreRicoPadrePobre.jpg',
         2,
         "Padre rico, padre pobre es un libro de finanzas personales escrito por Robert Kiyosaki y Sharon Lechter. Fue publicado por primera vez en 1997. El libro aboga por la importancia de la educación financiera, incluyendo la inversión, la gestión de activos, la formación de corporaciones y el uso de estrategias fiscales."),
     Libro(
-        'El hombre en\nbusca de sentido',
+        'El hombre en busca de sentido',
         'Viktor Frankl',
         'assets/images/librosPrueba/book_hombreEnBuscaDeSentido.jpg',
         3,
         "El hombre en busca de sentido es un libro de psicología escrito por Viktor Frankl. El libro relata sus experiencias como prisionero en campos de concentración durante la Segunda Guerra Mundial, y describe su método para encontrar un motivo para vivir."),
     Libro(
-        'Piense y\nhágase rico',
+        'Piense y hágase rico',
         'Napoleon Hill',
         'assets/images/librosPrueba/book_pienseYhagaseRico.jpg',
         5,
         "Piense y hágase rico es un libro de autoayuda escrito por Napoleon Hill. Fue publicado por primera vez en 1937. El libro está basado en una serie de entrevistas a Andrew Carnegie, que Hill realizó durante 25 años y que le sirvieron de base para su filosofía del éxito."),
     Libro(
-        'El poder\ndel ahora',
+        'El poder del ahora',
         'Eckhart Tolle',
         'assets/images/librosPrueba/book_poderDelAhora.jpg',
         4,
         "El poder del ahora es un libro de espiritualidad escrito por Eckhart Tolle. Fue publicado por primera vez en 1997. El libro defiende la importancia de la atención plena y la meditación en la vida diaria."),
     Libro(
-        'El monje que\nvendio su ferrari',
+        'El monje que vendio su ferrari',
         'Robin Sharma',
         'assets/images/librosPrueba/book_monjeQueVendioSuFerrari.jpeg',
         1,
@@ -55,7 +55,7 @@ class _TusLibros extends State<TusLibros> {
         4,
         "Hábitos atómicos es un libro de autoayuda escrito por James Clear. Fue publicado por primera vez en 2018. El libro defiende la importancia de los pequeños cambios en la vida diaria para lograr grandes resultados."),
     Libro(
-        'El poder de\nlas Palabras',
+        'El poder de las Palabras',
         'Mariano Sigman',
         'assets/images/librosPrueba/book_elPoderDeLasPalabras.jpg',
         3,
@@ -90,57 +90,64 @@ class _TusLibros extends State<TusLibros> {
     return Padding(
       padding: const EdgeInsets.all(10),
       child: GestureDetector(
-        onTap: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => InfoLibro(libro: listaLibros[i])));
-        },
-        child: Card(
-          elevation: 4,
-          color: MisColores.marronOscuro1,
-          child: SizedBox(
-            height: 200,
-            child: Row(
-              children: [
-                Padding(
-                    padding: const EdgeInsets.all(15),
-                    child: Image(
-                      image: AssetImage(listaLibros[i].imagen),
-                      width: 100,
-                      height: 150,
-                    )),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: Text(
-                        listaLibros[i].titulo,
-                        style: const TextStyle(
-                            color: Colors.black,
-                            fontSize: 20.5,
-                            fontWeight: FontWeight.bold),
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => InfoLibro(libro: listaLibros[i])));
+          },
+          child: Card(
+            color: MisColores.marronOscuro1,
+            child: SizedBox(
+              height: 200,
+              child: Row(
+                children: [
+                  Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Image(
+                        image: AssetImage(listaLibros[i].imagen),
+                        width: 100,
+                        height: 150,
+                      )),
+                  SizedBox(
+                    height: 200,
+                    width: MediaQuery.of(context).size.width - 180,
+                    child: Positioned(
+                      right: 0,
+                      left: 0,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(10),
+                            child: Text(
+                              textAlign: TextAlign.center,
+                              listaLibros[i].titulo,
+                              style: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20.5,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(10),
+                            child: Text(
+                              listaLibros[i].autor,
+                              style: const TextStyle(
+                                  fontFamily: 'InriaSerif',
+                                  color: Colors.black,
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.normal),
+                            ),
+                          )
+                        ],
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: Text(
-                        listaLibros[i].autor,
-                        style: const TextStyle(
-                            fontFamily: 'InriaSerif',
-                            color: Colors.black,
-                            fontSize: 17,
-                            fontWeight: FontWeight.normal),
-                      ),
-                    )
-                  ],
-                ),
-              ],
+                  )
+                ],
+              ),
             ),
-          ),
-        ),
-      ),
+          )),
     );
   }
 }
