219cf5ee02808e4769fc05623d3c153e36c24ea6
JulenR14 <144845185+JulenR14@users.noreply.github.com>
2024-03-10 17:31:40 +0100

    He arreglado lo que quedaba

--- mis_libros/lib/screens/views/buscar_libros.dart
+++ mis_libros/lib/screens/views/buscar_libros.dart
@@ -39,122 +39,92 @@ class _BuscarLibros extends State<BuscarLibros> {
             ],
           ),
         ),
-        /*ListView.builder(
-            itemCount: listaLibros.length,
-            itemBuilder: (BuildContext context, int index) {
-              return crearListaCards(index);
-            }),*/
+        SizedBox(
+          height: MediaQuery.of(context).size.height - 160,
+          child: ListView.builder(
+              itemCount: listaLibros.length,
+              itemBuilder: (BuildContext context, int index) {
+                return _crearListaCards(index);
+              }),
+        ),
       ]),
     );
   }
 
   Widget _crearListaCards(int i) {
-    return const Padding(
-      padding: EdgeInsets.all(10),
-      child: Card(
-        shadowColor: MisColores.nero,
-        color: MisColores.marronOscuro4,
-        child: SizedBox(
-          height: 200,
-          child: Stack(
-            children: [
-              Positioned(
-                right: 0,
-                left: 0,
-                child: Padding(
-                  padding: EdgeInsets.all(20),
-                  child: Text(
-                    "sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontFamily: 'RobotoSerif',
-                        color: MisColores.nero,
-                        fontSize: 20,
-                        fontWeight: FontWeight.normal),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget crearListaCards(int i) {
     return Padding(
-      padding: const EdgeInsets.all(10),
-      child: GestureDetector(
+      padding: EdgeInsets.all(10),
+      /*child: GestureDetector(
           onTap: () {
             /*Navigator.push(
                 context,
                 MaterialPageRoute(
                     builder: (context) => InfoLibro(libro: listaLibros[i])));*/
-          },
-          child: Card(
-            shadowColor: MisColores.nero,
-            color: MisColores.marronOscuro4,
-            child: SizedBox(
-              height: 200,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(15),
-                    //child: Hero(
-                    //tag: "${listaLibros[i].imagen}tuslibros",
-                    child: Image(
-                      image: AssetImage(listaLibros[i].imagen),
-                      width: 100,
-                      height: 150,
-                    ),
-                    //),
-                  ),
-                  SizedBox(
-                    height: 200,
-                    width: MediaQuery.of(context).size.width - 180,
-                    child: Stack(children: [
-                      Positioned(
-                        right: 0,
-                        left: 0,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(10),
-                              child: Hero(
-                                tag: listaLibros[i].titulo,
-                                child: Text(
-                                  listaLibros[i].titulo,
-                                  textAlign: TextAlign.center,
-                                  style: const TextStyle(
-                                      fontFamily: 'InriaSerif',
-                                      color: MisColores.marronOscuro1,
-                                      fontSize: 25,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(10),
-                              child: Text(
-                                listaLibros[i].autor,
-                                style: const TextStyle(
-                                    fontFamily: 'InriaSerif',
-                                    color: MisColores.marronOscuro1,
-                                    fontSize: 17,
-                                    fontWeight: FontWeight.normal),
-                              ),
-                            )
-                          ],
-                        ),
-                      )
-                    ]),
-                  )
-                ],
+          },*/
+      child: Card(
+        shadowColor: MisColores.nero,
+        color: MisColores.marronOscuro4,
+        child: SizedBox(
+          height: 200,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(15),
+                //child: Hero(
+                //tag: "${listaLibros[i].imagen}tuslibros",
+                child: Image(
+                  image: AssetImage(listaLibros[i].imagen),
+                  width: 100,
+                  height: 150,
+                ),
               ),
-            ),
-          )),
+              //),
+              SizedBox(
+                height: 200,
+                width: MediaQuery.of(context).size.width - 180,
+                child: Stack(children: [
+                  Positioned(
+                    right: 0,
+                    left: 0,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(10),
+                          //child: Hero(
+                          //tag: listaLibros[i].titulo,
+                          child: Text(
+                            listaLibros[i].titulo,
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                                fontFamily: 'InriaSerif',
+                                color: MisColores.marronOscuro1,
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          //),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Text(
+                            listaLibros[i].autor,
+                            style: const TextStyle(
+                                fontFamily: 'InriaSerif',
+                                color: MisColores.marronOscuro1,
+                                fontSize: 17,
+                                fontWeight: FontWeight.normal),
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ]),
+              )
+            ],
+          ),
+        ),
+      ), //),
     );
   }
 }
